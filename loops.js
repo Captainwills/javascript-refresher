b82b5c221f52a899411f2c476832e3c22f963e79
Captainwills <ohiozuagodswill@gmail.com>
2024-06-13 16:49:28 +0100

    13th June 2024

---
+++ loops.js
@@ -0,0 +1,10 @@
+// Using the while loop
+        let number = 3;
+        let counts ="";
+// Exceute until 500
+        while (number != 500) {
+            number = number += 1;
+            counts = counts+ number +  "\n";
+
+        }      
+        console.log(counts);
